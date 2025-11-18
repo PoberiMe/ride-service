2f0f314e21a7d768206ae3c59ce45c27ea04165c
Matjaž Auberšek <matjaz.aubersek@gmail.com>
2025-11-18 12:45:55 +0100

    setup

---
+++ src/test/java/me/poberi/rideservice/RideServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package me.poberi.rideservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RideServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
